00536c8f3f29f044402436ef143cb3177c7e2ca7
Aidan De Groot <aidanjdegroot@gmail.com>
2023-03-31 14:26:13 +0200

    website updated

---
+++ Website/index.php
@@ -0,0 +1,60 @@
+<!Doctype Html>
+<Html lang="en">
+<Head>
+    <Title>Tweets</Title>
+    <link rel="stylesheet" href="main.css">
+</Head>
+
+<Body>
+
+<nav>
+    <img class="website-logo" src="images/PHP-Project_Logo.png" alt="Logo">
+    <ul>
+        <li><a href="index.php">Homepage</a></li>
+        <li><a href="tweets.php">Tweets</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="profile.php">Account</a></li>
+    </ul>
+</nav>
+
+
+<form method="POST">
+    <label for="wat">wat:</label>
+    <input type="text" id="wat" name="content">
+    <br>
+    <label for="username">naam:</label>
+    <input type="text" id="username" name="username">
+    <br>
+    <input type="submit" value="tweet">
+</form>
+
+
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+if(isset($_POST["content"])){
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=chirpify", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+    //"INSERT INTO tweets (content, username)
+    //VALUES('coole eerste tweet', 'marcel')"
+    $x = $conn->prepare("INSERT INTO tweets (content, username)
+                    VALUES('{$_POST["content"]}', '{$_POST["username"]}')");
+
+    $x->execute();
+}
+?>
+
+</Body>
+</Html>
--- Website/profile.php
+++ Website/profile.php
@@ -10,21 +10,15 @@
 <nav>
     <img class="website-logo" src="images/PHP-Project_Logo.png" alt="Logo">
     <ul>
-<<<<<<< HEAD:Website/profile.php
         <li><a href="index.php">Homepage</a></li>
         <li><a href="tweets.php">Tweets</a></li>
         <li><a href="login.php">Login</a></li>
         <li><a href="profile.php">Account</a></li>
-=======
-        <li><a href="tweets.php">Homepage</a></li>
-        <li><a href="sign-up.php">Sign-up</a></li>
-        <li><a href="account.php">Account</a></li>
->>>>>>> main:Website/account.php
     </ul>
 </nav>
 
 <div class="profiel-container">
-    <h1>Naam</h1>
+    <h1>(Naam)</h1>
     <div class="profiel">
         <img class="profiel-foto" src="images/profiel-foto.png" alt="profielfoto">
         <h2>(Profiel-foto)</h2>
